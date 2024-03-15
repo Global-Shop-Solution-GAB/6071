9c6e7480f1e2fb6e00dd938e9c9bbfbfd8556c3a
Service Web <sw@gssmail.com>
2024-03-15 11:38:38 -0500

    Commit changes in C:\Websites\Git\emarlow\6071\GCG_6071_Selection.sql

---
+++ GCG_6071_Selection.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_6071_Selection"(
+ "BalGroup" CHAR(8),
+ "Shift" CHAR(8),
+ "RefreshTime" INTEGER,
+ "Dept" CHAR(7),
+ "Scroll" CHAR(3),
+ "EmailID" CHAR(100),
+ "EmailID2" CHAR(100),
+ "Notify" BIT DEFAULT '0' NOT NULL );
